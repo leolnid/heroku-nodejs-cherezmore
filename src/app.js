d7789a108ab463409e3e21dc82230e496b189d74
leo <leonid.dyukov@gmail.com>
2020-08-23 16:12:51 +0300

    Init commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const body = require('body');
+
+
+port = process.env.port || 3000;
+
+
+const app = express();
+app.use(morgan('dev'))
+
+
+app.get('/', (req, res, next) => {
+    res.json({
+        message: "Hello, World"
+    })
+})
+
+
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    
+    next(error);
+})
+
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        error: {
+            message: err.message,
+        }
+    })
+})
+
+
+app.listen(port, () => console.log(`Server started on port: ${port}`))
